b9602cefadbdda3b0e5ded3eb58237033c859868
masterhyl3n <89370157+masterhyl3n@users.noreply.github.com>
2023-03-01 17:47:21 -0800

    Add files via upload

---
+++ IPGeoloc.py
@@ -0,0 +1,29 @@
+# import module
+from geopy.geocoders import Nominatim
+
+# initialize Nominatim API
+geolocator = Nominatim(user_agent="geoapiExercises")
+
+
+# Latitude & Longitude input
+Latitude = "25.594095"
+Longitude = "85.137566"
+
+location = geolocator.reverse(Latitude+","+Longitude)
+
+# Display
+print(location)
+
+address = location.raw['address']
+print(address)
+
+# traverse the data
+city = address.get('city', '')
+state = address.get('state', '')
+country = address.get('country', '')
+code = address.get('country_code')
+zipcode = address.get('postcode')
+print('City : ', city)
+print('State : ', state)
+print('Country : ', country)
+print('Zip Code : ', zipcode)
